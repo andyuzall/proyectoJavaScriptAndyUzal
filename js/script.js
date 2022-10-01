fd8474bec03951c3d2851b722762cce3627e48d1
Andy Uzal <andyuzal@gmail.com>
2022-10-01 10:17:49 +0200

    Cambios de estilo y algoritmos JS

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Acceso de usuario y saludo//
+let ingresoUsuario = prompt("Ingrese su nombre: ");
+let ingresoEmail = prompt("Ingrese su E-mail: ");
+
+if (ingresoUsuario) {
+    alert("Bienvenido" + " " + ingresoUsuario + "," + " " 
+        + "tiene el acceso permitido");
+}else {
+    ingresoUsuario = prompt("Ingrese su nombre: ");
+    alert("Bienvenido" + " " + ingresoUsuario + "," + " " 
+        + "tiene el acceso permitido");
+}
+
+
+//Condicional FOR WHILE 
+
+
+
+
+// Function
+
+
+
