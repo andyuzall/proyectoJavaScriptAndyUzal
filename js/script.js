f796c358091f162c18f58b260001b4c2ae03290f
Andy Uzal <andyuzal@gmail.com>
2022-10-04 17:36:40 +0200

    Primer entrega proyecto final

--- js/script.js
+++ js/script.js
@@ -1,23 +1,54 @@
-// Acceso de usuario y saludo//
-let ingresoUsuario = prompt("Ingrese su nombre: ");
-let ingresoEmail = prompt("Ingrese su E-mail: ");
+/*
+Do While para registrar y dar la bienvenida al usuario, siempre y cuando
+cumpla con los requisitos (+ 18 edad)
+*/
 
-if (ingresoUsuario) {
-    alert("Bienvenido" + " " + ingresoUsuario + "," + " " 
-        + "tiene el acceso permitido");
-}else {
-    ingresoUsuario = prompt("Ingrese su nombre: ");
-    alert("Bienvenido" + " " + ingresoUsuario + "," + " " 
-        + "tiene el acceso permitido");
+let nombreUsuario = prompt("Ingrese un nombre: ");
+let mailUsuario = prompt("Ingrese un e-mail: ");
+let edadUsuario = 0;
+
+do {
+    edadUsuario = parseInt(prompt("Ingrese su edad: "));
+}while (edadUsuario < 18 );
+alert("Bienvenido" + " " + nombreUsuario);
+
+
+// Funcion para seleccionar el tipo de seccion. 
+function seleccionarSeccion (seccion, opciones) {
+
+    switch(seccion) {
+        case "Entretenimiento":
+            return("Usted eligio la seccion" + " " + seccion);
+            break;
+
+        case "Cine":
+            return("Usted eligio la seccion"  + " " + seccion);
+            break;
+
+        case "Deporte":
+            return("Usted eligio la seccion"  + " " + seccion);
+            break;
+
+        case "Diversion nocturna":
+            return("Usted eligio la seccion diversion"  + " " + seccion);
+            break;
+
+        case "Gastronomia":
+            return("Usted eligio la seccion"  + " " + seccion);
+            break;
+        
+        default:
+            return "Debe elegir una seccion";
+            break;
+    }
 }
 
+let seleccion = prompt("Ingrese la seccion deseada: ");
+let entr = seleccionarSeccion(seleccion, "seccion");
+alert(entr);
 
-//Condicional FOR WHILE 
+// Funcion para saludar en la seccion
 
-
-
-
-// Function
-
-
+ const saludo = (a,b) => a + b;
+ alert( saludo(nombreUsuario + "," + " " + "estas ubicado en la seccion de" + " ", seleccion) ) 
 
