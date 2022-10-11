24996c685ce47e644983aec73ebb078f51b59454
Andy Uzal <andyuzal@gmail.com>
2022-10-12 01:10:11 +0200

    Estilizacion

--- js/script.js
+++ js/script.js
@@ -3,52 +3,52 @@ Do While para registrar y dar la bienvenida al usuario, siempre y cuando
 cumpla con los requisitos (+ 18 edad)
 */
 
-let nombreUsuario = prompt("Ingrese un nombre: ");
-let mailUsuario = prompt("Ingrese un e-mail: ");
-let edadUsuario = 0;
+// let nombreUsuario = prompt("Ingrese un nombre: ");
+// let mailUsuario = prompt("Ingrese un e-mail: ");
+// let edadUsuario = 0;
 
-do {
-    edadUsuario = parseInt(prompt("Ingrese su edad: "));
-}while (edadUsuario < 18 );
-alert("Bienvenido" + " " + nombreUsuario);
+// do {
+//     edadUsuario = parseInt(prompt("Ingrese su edad: "));
+// }while (edadUsuario < 18 );
+// alert("Bienvenido" + " " + nombreUsuario);
 
 
 // Funcion para seleccionar el tipo de seccion. 
-function seleccionarSeccion (seccion, opciones) {
+// function seleccionarSeccion (seccion, opciones) {
 
-    switch(seccion) {
-        case "Entretenimiento":
-            return("Usted eligio la seccion" + " " + seccion);
-            break;
+//     switch(seccion) {
+//         case "Entretenimiento":
+//             return("Usted eligio la seccion" + " " + seccion);
+//             break;
 
-        case "Cine":
-            return("Usted eligio la seccion"  + " " + seccion);
-            break;
+//         case "Cine":
+//             return("Usted eligio la seccion"  + " " + seccion);
+//             break;
 
-        case "Deporte":
-            return("Usted eligio la seccion"  + " " + seccion);
-            break;
+//         case "Deporte":
+//             return("Usted eligio la seccion"  + " " + seccion);
+//             break;
 
-        case "Diversion nocturna":
-            return("Usted eligio la seccion diversion"  + " " + seccion);
-            break;
+//         case "Diversion nocturna":
+//             return("Usted eligio la seccion diversion"  + " " + seccion);
+//             break;
 
-        case "Gastronomia":
-            return("Usted eligio la seccion"  + " " + seccion);
-            break;
+//         case "Gastronomia":
+//             return("Usted eligio la seccion"  + " " + seccion);
+//             break;
         
-        default:
-            return "Debe elegir una seccion";
-            break;
-    }
-}
+//         default:
+//             return "Debe elegir una seccion";
+//             break;
+//     }
+// }
 
-let seleccion = prompt("Ingrese la seccion deseada: ");
-let entr = seleccionarSeccion(seleccion, "seccion");
-alert(entr);
+// let seleccion = prompt("Ingrese la seccion deseada: ");
+// let entr = seleccionarSeccion(seleccion, "seccion");
+// alert(entr);
 
 // Funcion para saludar en la seccion
 
- const saludo = (a,b) => a + b;
- alert( saludo(nombreUsuario + "," + " " + "estas ubicado en la seccion de" + " ", seleccion) ) 
+//  const saludo = (a,b) => a + b;
+//  alert( saludo(nombreUsuario + "," + " " + "estas ubicado en la seccion de" + " ", seleccion) ) 
 
